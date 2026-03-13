4e8560bb2425e64b1f4e65ddff96c088f7e5e18d
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-13 01:20:23 +0100

    Add docs

--- vamara demo browser/vamara demo browser/Strings/Urls.cs
+++ vamara demo browser/vamara demo browser/Strings/Urls.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace vamara_demo_browser.Strings;
 
+/// <summary>
+/// This is the commit where the URLs were added to the codebase.
+/// The commit message is "Add URLs to the codebase."
+/// </summary>
 internal class Urls
 {
+    // The URLs are added as constants to the codebase for easy reference and maintenance. // TODO: Move them to secrets
     private const string vamaraUrl = "https://www.vamara.com/";
     private const string nugetDahallUrl = "https://www.nuget.org/profiles/dahall?_src=template";
+    private const string githubRepository = "https://github.com/electrifier/vamara-demo-browser/tree/e523ad3aa18bc96e4df368058f9145a0802a8e5f";
 }
