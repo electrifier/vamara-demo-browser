543cfe405077ee905014a6fe0c688a476b8c8c62
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-13 13:31:03 +0100

    fix build issue

--- vamara demo browser/vamara demo browser/Pages/ClipboardPanel.xaml.cs
+++ vamara demo browser/vamara demo browser/Pages/ClipboardPanel.xaml.cs
@@ -18,11 +18,11 @@ namespace vamara_demo_browser.Pages;
 /// <summary>
 /// An empty window that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class BlankWindow1 : Window
+public sealed partial class ClipboardPanel : Window
 {
-    public BlankWindow1()
+    public ClipboardPanel()
     {
-        InitializeComponent();
+        //InitializeComponent();
     }
 }
 
