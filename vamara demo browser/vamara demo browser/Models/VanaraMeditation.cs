1c724c29ca54767cd3e77acfec2f7713e85bb2d4
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-13 04:43:07 +0100

    Overhaul `VanaraMeditation`

--- vamara demo browser/vamara demo browser/Models/VanaraMeditation.cs
+++ vamara demo browser/vamara demo browser/Models/VanaraMeditation.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace vamara_demo_browser.Models;
 
 /// <summary>
-/// 
+/// Vanara specific <inheritdoc cref="Exception"/>.
 /// </summary>
 internal class VanaraMeditation : Exception
 {
+    public VanaraMeditation() : base("Vanara Meditation: An unknown error occurred.")
+    {
+    }
+    public VanaraMeditation(string message, Exception innerException) : base($"Vanara Meditation: {message}", innerException)
+    {
+    }
+    public VanaraMeditation(string message, Exception innerException, CancellationToken cancellationToken) : base($"Vanara Meditation: {message}", innerException)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+    }
+    public VanaraMeditation(Exception message) : base($"Vanara Meditation: {message}")
+    {
+    }
+    public VanaraMeditation(string message) : base($"Vanara Meditation: {message}")
+    {
+    }
     //public VanaraMeditation(COMValue) : base($"Vanara COMValue Meditation: {COMValue}")
 }
