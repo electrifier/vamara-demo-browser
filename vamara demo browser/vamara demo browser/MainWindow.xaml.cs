26e0d1e385f3a1fcadac4c839e13488b1bfb39db
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-15 10:58:02 +0100

    Add gui prototypes. Refactoring.

--- vamara demo browser/vamara demo browser/MainWindow.xaml.cs
+++ vamara demo browser/vamara demo browser/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -49,11 +37,11 @@ public sealed partial class MainWindow : Window
 
         try
         {
-//                if (args.IsSettingsSelected)
-//                {
-//                    // Handle settings selection
-//                    return;
-//                }
+            //                if (args.IsSettingsSelected)
+            //                {
+            //                    // Handle settings selection
+            //                    return;
+            //                }
         }
         catch (Exception)
         {
