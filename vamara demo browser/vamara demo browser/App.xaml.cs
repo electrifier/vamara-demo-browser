a5beae22907da3575ce1c154cbc58fd58cf8c8ff
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-20 00:02:57 +0100

    Add `Vortice.WinUI` package. Refactor.

--- vamara demo browser/vamara demo browser/App.xaml.cs
+++ vamara demo browser/vamara demo browser/App.xaml.cs
@@ -8,54 +8,58 @@ using static Vanara.PInvoke.Ole32.PROPERTYKEY.System;
 
 namespace vamara_demo_browser;
 
-/// <summary>
-/// Provides application-specific behavior to supplement the default Application class.
-/// 
+/// <summary>Provides application-specific behavior to supplement the default Application class.
 /// <b>Quote:</b> <i>To learn more about WinUI, the WinUI project structure, and more about
-/// our project templates, see: http://aka.ms/winui-project-info.</i>
-/// </summary>
+/// our project templates, see: http://aka.ms/winui-project-info.</i></summary>
 public partial class App : Application
 {
-    public static IServiceProvider Services { get; private set; }
-
     public ServiceCollection ServiceCollection = new();
+    public static IServiceProvider Services { get; private set; }
 
     private Window? _window;
     public static Window MainWindow => ((App)Current)._window!;
 
-    /// <summary>
-    /// Initializes the singleton application object.  This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
-    /// </summary>
+    /// <summary>Initializes the singleton application object. This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().</summary>
     public App()
     {
         Debug.WriteLine("App.ctor()");
         InitializeComponent();
         ConfigureServices();
-
-        ServiceCollection.AddSingleton<ILocalizationService, LocalizationService>();
-        ServiceCollection.AddTransient<SettingsViewModel>();
-
     }
 
     private void ConfigureServices()
     {
-        //var services = new ServiceCollection();
+        try 
+        {
+            // Services
+            ServiceCollection.AddSingleton<ILocalizationService, LocalizationService>();
 
-        // Services
-        ServiceCollection.AddSingleton<ILocalizationService, LocalizationService>();
+            // ViewModels
+            ServiceCollection.AddTransient<SettingsViewModel>();
 
-        // ViewModels
-        ServiceCollection.AddTransient<SettingsViewModel>();
-
-        Services = ServiceCollection.BuildServiceProvider();
+            // Build the service provider
+            Services = ServiceCollection.BuildServiceProvider();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"App.ConfigureServices(): Error configuring services: {ex}");
+            throw;
+        }
     }
 
     /// <summary>Invoked when the application is launched.</summary>
     /// <param name="args">Details about the launch request and process.</param>
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
-        _window = new MainWindow();
-        _window.Activate();
+        try
+        {
+            _window = new MainWindow();
+            _window.Activate();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
     }
 }
