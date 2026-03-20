cdc225d47bfdc3fc615950491e3940001df5b33f
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-20 12:18:15 +0100

    Refactoring

--- vamara demo browser/vamara demo browser/Controls/Models/ShellFolderModel.cs
+++ vamara demo browser/vamara demo browser/Controls/Models/ShellFolderModel.cs
@@ -8,4 +8,17 @@ namespace vamara_demo_browser.Controls.Models;
 
 internal class ShellFolderModel
 {
+    string _name;
+    string _description;
+    StorageItemTypes _type;
+    // Bitmap _icon;
+    object _attributes;
+
+    internal ShellFolderModel(string name, string description, StorageItemTypes type, object attributes)
+    {
+        _name = name;
+        _description = description;
+        _type = type;
+        _attributes = attributes;
+    }
 }
