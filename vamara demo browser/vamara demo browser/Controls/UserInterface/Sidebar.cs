e032370ed3d4cfdda59d3354a1be0b7cf6d695ef
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-20 05:46:55 +0100

    Add more control stubs and protytypes. Reorganize

--- vamara demo browser/vamara demo browser/Controls/UserInterface/Sidebar.cs
+++ vamara demo browser/vamara demo browser/Controls/UserInterface/Sidebar.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace vamara_demo_browser.Controls;
+namespace vamara_demo_browser.Controls.UserInterface;
 
 public class Sidebar
 {
---
+++ vamara demo browser/vamara demo browser/Pages/FileExplorerPage.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace vamara_demo_browser.Pages;
+
+/// <summary>
+/// An FileExplorer page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class FileExplorerPage : Page
+{
+    public FileExplorerPage()
+    {
+        InitializeComponent();
+    }
+}
