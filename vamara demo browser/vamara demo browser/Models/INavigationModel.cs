8e8ddbd43241c7c8d05e006b87049a22338e7712
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-20 03:30:58 +0100

    Add `NavigationModel.cs` Add `INavigationModel.cs`

--- vamara demo browser/vamara demo browser/Models/INavigationModel.cs
+++ vamara demo browser/vamara demo browser/Models/INavigationModel.cs
@@ -4,11 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace vamara_demo_browser.Controls.Shell32;
+namespace vamara_demo_browser.Models;
 
-public class ExplorerBrowser
+internal class INavigationModel
 {
-    public ExplorerBrowser()
-    {
-    }
 }
--- vamara demo browser/vamara demo browser/Models/Shell32/NavigationModel.cs
+++ vamara demo browser/vamara demo browser/Models/Shell32/NavigationModel.cs
@@ -4,11 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace vamara_demo_browser.Controls.Shell32;
+namespace vamara_demo_browser.Models.Shell32;
 
-public class ExplorerBrowser
+internal class NavigationModel
 {
-    public ExplorerBrowser()
-    {
-    }
 }
