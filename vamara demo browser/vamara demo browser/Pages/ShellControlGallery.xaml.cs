4174c1f4c140ff7138745da354e61a288e1d700b
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-20 04:22:41 +0100

    Cleanup

--- vamara demo browser/vamara demo browser/Pages/ShellControlGallery.xaml.cs
+++ vamara demo browser/vamara demo browser/Pages/ShellControlGallery.xaml.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml.Controls;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using vamara_demo_browser.Models;
 
 namespace vamara_demo_browser.Pages;
 
-internal class ShellControlGallery
+public sealed partial class ShellControlGallery : Page
 {
+//    public SettingsViewModel ViewModel { get; }
 }
