c6d960c2e17f06a0deea65b5c3cff49415a1dd4b
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-20 11:33:24 +0100

    Add Shell Item Models

---
+++ vamara demo browser/vamara demo browser/Controls/Models/ShellFolderModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vamara_demo_browser.Controls.Models;
+
+internal class ShellFolderModel
+{
+}
