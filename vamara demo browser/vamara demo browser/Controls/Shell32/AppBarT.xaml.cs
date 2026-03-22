45fbcd895425a54b241613edca3cfac62ea9300c
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-22 08:53:03 +0100

    Refactor `AppBarT` Overhaul Icon Ressources

--- vamara demo browser/vamara demo browser/Controls/Shell32/AppBarT.xaml.cs
+++ vamara demo browser/vamara demo browser/Controls/Shell32/AppBarT.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
 
 namespace vamara_demo_browser.Controls.Shell32;
 
--- vamara demo browser/vamara demo browser/Controls/Shell32/BreadcrumbBar.xaml.cs
+++ vamara demo browser/vamara demo browser/Controls/Shell32/BreadcrumbBar.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
+using Microsoft.UI.Xaml.Controls;
 
 namespace vamara_demo_browser.Controls.Shell32;
 
